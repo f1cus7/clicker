a99bdf3e67f90f129624d6b76b1d26aa7b675ac5
f1cus7 <f1cus7@mail.ru>
2024-10-24 02:18:40 +0300

    0.3 русификация + улучшение стилей + поправка баланса

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const scoreNode = document.getElementById('score');
 
 const upgrades = {
   click: [
-    { btn: 'update-1', cost: 10, bonus: 1.34, increment: 2 },
-    { btn: 'update-2', cost: 100, bonus: 2.66, increment: 5 },
-    { btn: 'update-3', cost: 500, bonus: 6.5, increment: 5 },
-    { btn: 'update-4', cost: 1000, bonus: 13, increment: 5 },
-    { btn: 'update-5', cost: 5000, bonus: 35, increment: 5 }
+    { btn: 'update-1', cost: 10, bonus: 0.34, increment: 1 },
+    { btn: 'update-2', cost: 100, bonus: 1.66, increment: 3 },
+    { btn: 'update-3', cost: 500, bonus: 3, increment: 5 },
+    { btn: 'update-4', cost: 1000, bonus: 5, increment: 7 },
+    { btn: 'update-5', cost: 5000, bonus: 10, increment: 10 }
   ],
   auto: [
-    { btn: 'auto-update-1', cost: 100, bonus: 1, increment: 1 },
-    { btn: 'auto-update-2', cost: 1000, bonus: 2, increment: 1 },
-    { btn: 'auto-update-3', cost: 5000, bonus: 4, increment: 1 },
-    { btn: 'auto-update-4', cost: 10000, bonus: 6, increment: 1 },
-    { btn: 'auto-update-5', cost: 50000, bonus: 10, increment: 1 }
+    { btn: 'auto-update-1', cost: 100, bonus: 0.5, increment: 1 },
+    { btn: 'auto-update-2', cost: 1000, bonus: 2, increment: 3 },
+    { btn: 'auto-update-3', cost: 5000, bonus: 4, increment: 5 },
+    { btn: 'auto-update-4', cost: 10000, bonus: 6, increment: 7 },
+    { btn: 'auto-update-5', cost: 50000, bonus: 10, increment: 10 }
   ]
 };
 
