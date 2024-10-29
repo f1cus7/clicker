8bdc400e5d66e4d78769c166ed9763b6ef3c398a
f1cus7 <f1cus7@mail.ru>
2024-10-29 17:51:03 +0300

    0.7.1 небольшое изменение интерфейса

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const scoreNode = document.getElementById('score');
 const upgrades = {
   click: [
     { btn: 'update-1', cost: 10, bonus: 0.34, increment: 1, scale: 'prirost1', skyscaperNode: 'skyscaper-1' },
-    { btn: 'update-2', cost: 100, bonus: 1.66, increment: 3, scale: 'prirost2', skyscaperNode: 'skyscaper-2' },
-    { btn: 'update-3', cost: 500, bonus: 3, increment: 5, scale: 'prirost3', skyscaperNode: 'skyscaper-3' },
-    { btn: 'update-4', cost: 1000, bonus: 5, increment: 7, scale: 'prirost4', skyscaperNode: 'skyscaper-4' },
-    { btn: 'update-5', cost: 5000, bonus: 10, increment: 10, scale: 'prirost5', skyscaperNode: 'skyscaper-5' }
+    { btn: 'update-2', cost: 100, bonus: 1.66, increment: 3, scale: 'prirost1', skyscaperNode: 'skyscaper-2' },
+    { btn: 'update-3', cost: 500, bonus: 3, increment: 5, scale: 'prirost1', skyscaperNode: 'skyscaper-3' },
+    { btn: 'update-4', cost: 1000, bonus: 5, increment: 7, scale: 'prirost1', skyscaperNode: 'skyscaper-4' },
+    { btn: 'update-5', cost: 5000, bonus: 10, increment: 10, scale: 'prirost1', skyscaperNode: 'skyscaper-5' }
   ],
   auto: [
     { btn: 'auto-update-1', cost: 100, bonus: 0.5, increment: 1, scale: 'auto-prirost1' },
-    { btn: 'auto-update-2', cost: 1000, bonus: 2, increment: 3, scale: 'auto-prirost2' },
-    { btn: 'auto-update-3', cost: 5000, bonus: 4, increment: 5, scale: 'auto-prirost3' },
-    { btn: 'auto-update-4', cost: 10000, bonus: 6, increment: 7, scale: 'auto-prirost4' },
-    { btn: 'auto-update-5', cost: 50000, bonus: 10, increment: 10, scale: 'auto-prirost5' }
+    { btn: 'auto-update-2', cost: 1000, bonus: 2, increment: 3, scale: 'auto-prirost1' },
+    { btn: 'auto-update-3', cost: 5000, bonus: 4, increment: 5, scale: 'auto-prirost1' },
+    { btn: 'auto-update-4', cost: 10000, bonus: 6, increment: 7, scale: 'auto-prirost1' },
+    { btn: 'auto-update-5', cost: 50000, bonus: 10, increment: 10, scale: 'auto-prirost1' }
   ]
 };
 
