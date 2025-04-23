ad30ad18cbb4903e7f454c7454e30452050b26ad
f1cus7 <f1cus7@mail.ru>
2025-04-23 03:39:29 +0300

    0.2 добавил новые вкладки, осталась небольшая полировка перед js

--- scripts/click-audio.js
+++ scripts/click-audio.js
@@ -1,7 +1,11 @@
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.querySelectorAll('img').forEach(img => {
+//     img.setAttribute('draggable', 'false');
+//   });
+
 const mainZarabotokClick = document.querySelector('.main-zarabotok-click');
 
 mainZarabotokClick.addEventListener('click', () => {
-    const clickSound = new Audio('audio/click1.mp3');
+    const clickSound = new Audio('audio/click.mp3');
     clickSound.play();
 });
-
--- scripts/test.js
+++ scripts/test.js
@@ -1,21 +1,76 @@
 const pageMain = document.querySelector('.page-main');
 const pageBusiness = document.querySelector('.page-business');
+const pageItems = document.querySelector('.page-items');
+const pageCards = document.querySelector('.page-cards');
+const pageStat = document.querySelector('.page-stat');
 
 const btnPageMain = document.getElementById('btn-page-main');
 const btnPageBusiness = document.getElementById('btn-page-business');
-
-
-btnPageBusiness.addEventListener('click', () => {
-    pageMain.classList.add('d-none');
-    pageBusiness.classList.remove('d-none');
-    btnPageMain.classList.remove('navigation-btn-active');
-    btnPageBusiness.classList.add('navigation-btn-active');
-})
+const btnPageItems = document.getElementById('btn-page-items');
+const btnPageCards = document.getElementById('btn-page-cards');
+const btnPageStat = document.getElementById('btn-page-stat');
 
 btnPageMain.addEventListener('click', () => {
     pageMain.classList.remove('d-none');
     pageBusiness.classList.add('d-none');
+    pageItems.classList.add('d-none');
+    pageCards.classList.add('d-none');
+    pageStat.classList.add('d-none');
     btnPageMain.classList.add('navigation-btn-active');
     btnPageBusiness.classList.remove('navigation-btn-active');
+    btnPageItems.classList.remove('navigation-btn-active');
+    btnPageCards.classList.remove('navigation-btn-active');
+    btnPageStat.classList.remove('navigation-btn-active');
 })
 
+btnPageBusiness.addEventListener('click', () => {
+    pageMain.classList.add('d-none');
+    pageBusiness.classList.remove('d-none');
+    pageItems.classList.add('d-none');
+    pageCards.classList.add('d-none');
+    pageStat.classList.add('d-none');
+    btnPageMain.classList.remove('navigation-btn-active');
+    btnPageBusiness.classList.add('navigation-btn-active');
+    btnPageItems.classList.remove('navigation-btn-active');
+    btnPageCards.classList.remove('navigation-btn-active');
+    btnPageStat.classList.remove('navigation-btn-active');
+})
+
+btnPageItems.addEventListener('click', () => {
+    pageMain.classList.add('d-none');
+    pageBusiness.classList.add('d-none');
+    pageItems.classList.remove('d-none');
+    pageCards.classList.add('d-none');
+    pageStat.classList.add('d-none');
+    btnPageMain.classList.remove('navigation-btn-active');
+    btnPageBusiness.classList.remove('navigation-btn-active');
+    btnPageItems.classList.add('navigation-btn-active');
+    btnPageCards.classList.remove('navigation-btn-active');
+    btnPageStat.classList.remove('navigation-btn-active');
+})
+
+btnPageCards.addEventListener('click', () => {
+    pageMain.classList.add('d-none');
+    pageBusiness.classList.add('d-none');
+    pageItems.classList.add('d-none');
+    pageCards.classList.remove('d-none');
+    pageStat.classList.add('d-none');
+    btnPageMain.classList.remove('navigation-btn-active');
+    btnPageBusiness.classList.remove('navigation-btn-active');
+    btnPageItems.classList.remove('navigation-btn-active');
+    btnPageCards.classList.add('navigation-btn-active');
+    btnPageStat.classList.remove('navigation-btn-active');
+})
+
+btnPageStat.addEventListener('click', () => {
+    pageMain.classList.add('d-none');
+    pageBusiness.classList.add('d-none');
+    pageItems.classList.add('d-none');
+    pageCards.classList.add('d-none');
+    pageStat.classList.remove('d-none');
+    btnPageMain.classList.remove('navigation-btn-active');
+    btnPageBusiness.classList.remove('navigation-btn-active');
+    btnPageItems.classList.remove('navigation-btn-active');
+    btnPageCards.classList.remove('navigation-btn-active');
+    btnPageStat.classList.add('navigation-btn-active');
+})
